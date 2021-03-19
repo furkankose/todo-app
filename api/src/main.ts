db80ccbb3343b88c5d7db9889e65367efad63090
furkankose <furkankosee@hotmail.com.tr>
2021-03-19 23:21:24 +0300

    feat(api): add global exception handlers

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './filters/httpException.filter';
+import { AnyExceptionFilter } from './filters/anyException.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.useGlobalFilters(new AnyExceptionFilter());
+  app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
