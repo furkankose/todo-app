ec3fddd16d4bcf77cd42730e3fc9fcbb64f857e4
furkankose <furkankosee@hotmail.com.tr>
2021-03-19 23:12:19 +0300

    chore(api): add dotenv package

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(process.cwd(), '.env') });
 
 @Module({
   imports: [],
