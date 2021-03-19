a208b35680bebd8a41f1e04f208e0a5198eff72d
furkankose <furkankosee@hotmail.com.tr>
2021-03-19 23:32:49 +0300

    feat(api): add mongodb connection

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { config } from 'dotenv';
 import { resolve } from 'path';
 
 config({ path: resolve(process.cwd(), '.env') });
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.TODO_MONGODB_CONNECTION_STRING)],
 })
 export class AppModule {}
