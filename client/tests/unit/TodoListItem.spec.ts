4dea0d75f29e32e361df8549d3cce9f3a741ae9f
furkankose <furkankosee@hotmail.com.tr>
2021-03-20 09:53:06 +0300

    test(client): add unit tests of TodoListItem

---
+++ client/tests/unit/TodoListItem.spec.ts
@@ -0,0 +1,48 @@
+import { shallowMount } from '@vue/test-utils';
+import TodoListItem from '@/components/TodoListItem.vue';
+
+describe('TodoListItem.vue', () => {
+  const title = 'Another todo';
+  const isCompleted = true;
+
+  it('renders incompleted todo title', () => {
+    const wrapper = shallowMount(TodoListItem, {
+      propsData: { title, isCompleted: false },
+    });
+
+    const titleClasses = wrapper.find('.todo-list-item-text').classes();
+    expect(titleClasses.includes('completed')).toEqual(false);
+  });
+
+  it('renders completed todo title', () => {
+    const wrapper = shallowMount(TodoListItem, {
+      propsData: { title, isCompleted },
+    });
+
+    const titleClasses = wrapper.find('.todo-list-item-text').classes();
+    expect(titleClasses.includes('completed')).toEqual(true);
+  });
+
+  it('emits complete event', async () => {
+    const expectedValue = !isCompleted;
+    const wrapper = shallowMount(TodoListItem, {
+      propsData: { title, isCompleted },
+    });
+    const checkbox = wrapper.find('input');
+
+    (checkbox.element as HTMLInputElement).checked = expectedValue;
+    checkbox.trigger('change');
+
+    expect(wrapper.emitted().complete[0][0]).toEqual(expectedValue);
+  });
+
+  it('emits delete event', async () => {
+    const wrapper = shallowMount(TodoListItem, {
+      propsData: { title, isCompleted },
+    });
+    const button = wrapper.find('.todo-list-item-button');
+
+    button.trigger('click');
+    expect(wrapper.emitted().delete).toBeDefined;
+  });
+});
