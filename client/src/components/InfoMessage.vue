514f7049c7d602740251b3d51ee484dda30dbaf5
furkankose <furkankosee@hotmail.com.tr>
2021-03-21 11:58:53 +0300

    feat(client): add info message component

---
+++ client/src/components/InfoMessage.vue
@@ -0,0 +1,27 @@
+<template>
+  <span :style="{ color, fontSize }">
+    <slot></slot>
+  </span>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'InfoMessage',
+  props: {
+    color: {
+      type: String,
+      default: '#c4c4c4',
+    },
+    fontSize: {
+      type: String,
+      default: '32px',
+    },
+  },
+});
+</script>
+<style scoped>
+span {
+  display: block;
+}
+</style>
