c04d0368437a2fae30fc9963729f697c81f7f58b
furkankose <furkankosee@hotmail.com.tr>
2021-03-21 11:56:47 +0300

    feat(client): add preloader component

---
+++ client/src/components/Preloader.vue
@@ -0,0 +1,21 @@
+<template>
+  <i class="fas fa-spinner fa-pulse" :style="{ color, fontSize: size }"></i>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Preloader',
+  props: {
+    color: {
+      type: String,
+      default: '#c4c4c4',
+    },
+    size: {
+      type: String,
+      default: '32px',
+    },
+  },
+});
+</script>
+<style scoped></style>
