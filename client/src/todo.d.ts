039a0522f80e69d0a4595d6e3dafe517e9249066
furkankose <furkankosee@hotmail.com.tr>
2021-03-23 00:37:08 +0300

    fix(client): fix type error

--- client/src/todo.d.ts
+++ client/src/todo.d.ts
@@ -1,3 +1,11 @@
+import { SnotifyService } from 'vue-snotify/SnotifyService';
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $snotify: SnotifyService;
+  }
+}
+
 export interface Todo {
   _id?: string;
   title: string;
