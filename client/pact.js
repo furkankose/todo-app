855fc2cd5590bc932535f40380928843def8eb77
furkankose <furkankosee@hotmail.com.tr>
2021-03-22 23:22:51 +0300

    docs: update README

--- client/pact.js
+++ client/pact.js
@@ -4,8 +4,10 @@ const packageJson = require('./package.json');
 const path = require('path');
 const opts = {
   pactFilesOrDirs: [path.resolve(process.cwd(), 'pact/pacts')],
-  pactBroker: process.env.TODO_PACT_BROKER,
-  pactBrokerToken: process.env.TODO_PACT_BROKER_TOKEN,
+  // pactBroker: process.env.TODO_PACT_BROKER,
+  // pactBrokerToken: process.env.TODO_PACT_BROKER_TOKEN,
+  pactBroker: 'https://furkan.pactflow.io',
+  pactBrokerToken: 'ZRrHX0z8F3QpY9gdcCxB_Q',
   consumerVersion: packageJson.version,
 };
 
